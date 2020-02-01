36041eabe4cf32b40afad6222bc24feeb92ecb4a
Dave <dave.gagnat@gmail.com>
2020-01-31 18:20:33 -0600

    Added additional actions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root' # for front end
 
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:show, :create, :destroy]
+    resources :users, only: [:show, :edit, :create, :destroy]
     resource :session, only: [:create, :destroy]
   end
 end
