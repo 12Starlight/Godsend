35be802dbeec052a77ddbdbeb08f5a0690f56e81
Dave <dave.gagnat@gmail.com>
2020-01-31 19:26:30 -0600

    Tested jbuilder view see, if the show UsersController action worked, it did

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root' # for front end
-
+  
   namespace :api, defaults: { format: :json } do 
     resources :users, only: [:show, :edit, :create, :destroy]
     resource :session, only: [:create, :destroy]
   end
+  
 end
