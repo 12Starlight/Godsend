28ede9f9356cdf185831602ec7ad3315609c6bab
Dave <dave.gagnat@gmail.com>
2020-02-10 14:01:25 -0600

    Created signUpFormContainer

---
+++ frontend/session_form/signup_form_container.jsx
@@ -0,0 +1,25 @@
+// Import React Redux
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// Import Local Directory Files
+import { signUp } from '../actions/session_actions'; 
+import SignUp from './session_form';
+
+
+// mapStateToProps
+const mapStateToProps = ({ signUpErrors }) => ({
+  errors: signUpErrors.session,
+  formType: 'signUp',
+  navLink: <Link to="/login">{'Sign In :)'}</Link>
+});
+
+// mapDispatchToProps
+const mapDispatchToProps = (dispatch) => ({
+  demoAction: (demo) => dispatch(signUp(demo)),
+  userAction: (user) => dispatch(signUp(user))
+}); 
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
