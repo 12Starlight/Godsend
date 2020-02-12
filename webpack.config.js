c858456e158edc32ea3421452844224c59a72271
Dave <dave.gagnat@gmail.com>
2020-02-12 16:47:26 -0600

    Updated gitignore and changed webpack to include context

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 
 module.exports = {
+  context: __dirname,
   entry: './frontend/godsend.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
