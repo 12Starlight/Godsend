4d94613b04a3efc11e49ea886edf30c173eff879
Dave <dave.gagnat@gmail.com>
2020-02-14 03:45:37 -0600

    Created watchlists_api_util functions

---
+++ frontend/utils/watchlists_api_util.js
@@ -0,0 +1,28 @@
+export const receiveWatchlists = () => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/watchlists`
+  })
+);
+
+export const receiveWatchlist = (id) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/watchlists/${id}`
+  })
+);
+
+export const createWatchlist = (watchlist) => (
+  $.ajax({
+    method: `POST`,
+    url: `/api/watchlists`,
+    data: { watchlist }
+  })
+);
+
+export const deleteWatchlist = (id) => (
+  $.ajax({
+    method: `DELETE`,
+    url: `/api/watchlists/${id}`
+  })
+);
