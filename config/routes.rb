305582cdfe9946ceb1d280076c86760fced66245
Dave <dave.gagnat@gmail.com>
2020-02-13 20:59:11 -0600

    Created routes for watchlists and nested :index under users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root to: 'static_pages#root' # for front end
   
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:show, :edit, :create, :destroy]
+    resources :users, only: [:show, :edit, :create, :destroy] do 
+      resources :watchlists, only: [:index]
+    end
     resource :session, only: [:create, :destroy]
+    resources :watchlists, only: [:create, :destroy]
   end
   
 end
