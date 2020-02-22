c1b8587429ff1330735ef05a1d4203da7dcc8bfc
Dave <dave.gagnat@gmail.com>
2020-02-22 02:22:53 -0600

    Built stock util_api to grab news for a particular stock

--- frontend/utils/stock_util.js
+++ frontend/utils/stock_util.js
@@ -1,9 +1,18 @@
+// About 
 export const giveMeMyStock = (stockSearch) => (
   $.ajax({
     type: 'GET',
     url: `https://sandbox.iexapis.com/stable/stock/${stockSearch}/company?token=Tsk_a67dbc04df1e4d4291c42cc6c261a96c`
   })
 );
+
+// News
+export const giveMeMyStockNews = (stockSearch) => (
+  $.ajax({
+    type: 'GET',
+    url: `https://sandbox.iexapis.com/stable/stock/${stockSearch}/news?token=Tsk_a67dbc04df1e4d4291c42cc6c261a96c`
+  })
+); 
 
 
 
