156756e421d6a7de7b48078da0de39669873cc34
Dave <dave.gagnat@gmail.com>
2020-02-22 02:29:30 -0600

    Built stock util_api to grab analyst ratings for a particular stock

--- frontend/utils/stock_util.js
+++ frontend/utils/stock_util.js
@@ -14,5 +14,11 @@ export const giveMeMyStockNews = (stockSearch) => (
   })
 ); 
 
-
+// Analyst Ratings
+export const giveMeMYStockRatings = (stockSearch) => (
+  $.ajax({
+    type: 'GET',
+    url: `https://sandbox.iexapis.com/stable/stock/${stockSearch}/recommendation-trends?token=Tsk_a67dbc04df1e4d4291c42cc6c261a96c`
+  })
+);
 
