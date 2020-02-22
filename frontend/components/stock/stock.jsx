9a624399cee79f88d79eeaaec4f5904c14c7c8d6
Dave <dave.gagnat@gmail.com>
2020-02-22 01:43:03 -0600

    Created skeleton for stock container and stock presentational components

---
+++ frontend/components/stock/stock.jsx
@@ -0,0 +1,21 @@
+// Import React Redux
+import React from 'react';
+
+// Import Local Directory Files
+
+
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render(){
+    return(
+      <div>Give Me My Stock!</div>
+    )
+  }
+}
+
+
+export default Stock; 
---
+++ frontend/components/stock/stock_container.js
@@ -0,0 +1,18 @@
+// Import React Redux
+import { connect } from 'react-redux'; 
+
+// Import Local Directory Files
+import Stock from './stock';
+
+
+// mapStateToProps
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stock); 
