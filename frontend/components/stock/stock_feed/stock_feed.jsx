84a3c4b7323a710cd4cac7a8c5149982d85cac47
Dave <dave.gagnat@gmail.com>
2020-02-23 22:50:27 -0600

    Built folders for stock feed's chart, about, stock_news, analyst, stock_earnings, and also styling

--- frontend/components/stock/stock_feed/stock_feed.jsx
+++ frontend/components/stock/stock_feed/stock_feed.jsx
@@ -1,6 +1,14 @@
 // Import React Redux
 import React from 'react';
 
+// Import Local Directory Files
+import ChartContainer from '../stock_feed/chart/chart_container'
+import AboutContainer from '../stock_feed/about/about_container';
+import StockNewsContainer from '../stock_feed/stock_news/stock_news_container';
+import AnalystContainer from '../stock_feed/analyst/analyst_container';
+import StockEarningsContainer from '../stock_feed/stock_earnings/stock_earnings_container';
+import AlsoContainer from '../stock_feed/also/also_container';
+
 
 class StockFeed extends React.Component {
   constructor(props) {
@@ -15,6 +23,12 @@ class StockFeed extends React.Component {
   render() {
     return(
       <div className='stockFeed_container' >
+        <ChartContainer />
+        <AboutContainer />
+        <StockNewsContainer />
+        <AnalystContainer />
+        <StockEarningsContainer />
+        <AlsoContainer />
       </div>
     )
   }
