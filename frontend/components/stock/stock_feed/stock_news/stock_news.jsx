8536e7386c6767aa66d0256e2102b666f0fc3bc8
Dave <dave.gagnat@gmail.com>
2020-02-25 17:18:12 -0600

    Updated stockNews to incorporate stock news data

--- frontend/components/stock/stock_feed/stock_news/stock_news.jsx
+++ frontend/components/stock/stock_feed/stock_news/stock_news.jsx
@@ -1,5 +1,6 @@
 // Import React Redux
 import React from 'react';
+import { withRouter } from 'react-router-dom'; 
 
 // Import Local Directory Files
 
@@ -13,13 +14,63 @@ class StockNews extends React.Component {
     }
   }
 
+  componentDidMount() {
+    // debugger; 
+    this.props.giveMeMyStockNews(this.props.match.params.ticker);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      // console.log('Now I am on fire')
+      // console.log(this.props.match.params);
+      // debugger; 
+      this.props.giveMeMyStockNews(this.props.match.params.ticker);
+      // this.props.giveMeMyStockRatings(this.props.match.params.ticker);
+      // this.props.giveMeMyStockEarnings(this.props.match.params.ticker);
+      // this.props.givePeopleAlsoBought(this.props.match.params.ticker); 
+      this.props.history.push(`/stock/${this.props.match.params.ticker}`);
+    }
+  }
+
 
   render() {
+    const { stockNews } = this.props; 
+
+    if (!stockNews) {
+      return <div></div>;
+    }
+
+    const allNews = stockNews.map((newsItem, i) => {
+    debugger; 
+    const converted = convert(newsItem.datetime)
+
+      return (
+        <article key={i} className='feed_newsItem_container' >
+          <a href={newsItem.url} className='feed_newsItem_container_inner'>
+            <div className='feed_newsItem_data'>
+              <div className='feed_newsItem_content_container'>
+                <div className='feed_newsItem_source'>{newsItem.source}</div>
+                <div className='feed_newsItem_time'>{converted.hour}h</div>
+              </div>
+              <div className='feed_newsItem_headline_container'>
+                <div className='feed_newsItem_headline'>{newsItem.headline}</div>
+              </div>
+            </div>
+            <div className='feed_newsItem_pic_container'>
+              <img className='feed_newsItem_pic' src={newsItem.image} />
+            </div>
+          </a>
+        </article>
+      )
+    })
+
     return(
-      <div className='stockNews_container' >Stock News</div>
+      <div className='stockNews_container' >Stock News
+        {allNews}
+      </div>
     )
   }
 }
 
 
-export default StockNews; 
+export default withRouter(StockNews); 
--- frontend/components/stock/stock_feed/stock_news/stock_news_container.js
+++ frontend/components/stock/stock_feed/stock_news/stock_news_container.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux';
 
 // Import Local Directory Files
 import StockNews from './stock_news';
+import { giveMeMyStockNews } from '../../../../actions/stock_actions';
 
 
 // mapStateToProps
 const mapStateToProps = (state) => ({
-
+  stockNews: state.entities.stock.stockNews
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  giveMeMyStockNews: (searchTerm) => dispatch(giveMeMyStockNews(searchTerm))
 });
 
 
