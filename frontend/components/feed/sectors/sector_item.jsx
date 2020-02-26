132cfbb62097ed6626751de306668ff421c19777
Dave <dave.gagnat@gmail.com>
2020-02-25 19:40:10 -0600

    Built stockNews and stockNews item articles and styled to completion

--- frontend/components/feed/sectors/sector_item.jsx
+++ frontend/components/feed/sectors/sector_item.jsx
@@ -12,7 +12,10 @@ class SectorItem extends React.Component {
 
     this.valueEnter = this.valueEnter.bind(this);
     this.valueExit = this.valueExit.bind(this); 
+    this.valueEnterRate = this.valueEnterRate.bind(this);
+    this.valueExitRate = this.valueExitRate.bind(this); 
     this.value = React.createRef();
+    this.valueRate = React.createRef(); 
   }
   
   valueEnter(e, sector) {
@@ -21,11 +24,13 @@ class SectorItem extends React.Component {
     if (this.state.logic === false) {
       if (sector.performance <= 0) {
         // this.value.style.background = 'rgba(129, 71, 0, .822)';
+        // this.value.style.color = 'rgb(141, 15, 15)'; 
         this.value.style.background = '#ffffff';
         this.value.style.boxShadow = 'inset 0px 0px 12px 1px rgba(194,0,0,1)';
         this.value.style.border = 'inset 2.5px darkred';
       } else {
         // this.value.style.background = 'rgba(100, 0, 231, 0.822)';
+        // this.value.style.color = 'rgb(12, 107, 12)';
         this.value.style.background = '#ffffff';
         this.value.style.boxShadow = 'inset 0px 0px 12px 1px rgba(0,112,7,1)';
         this.value.style.border = 'inset 2.5px darkgreen'
@@ -50,14 +55,37 @@ class SectorItem extends React.Component {
     })
   }
 
+  valueEnterRate(e, sector) {
+    e.preventDefault();
+
+    if (this.state.logic === false) {
+      if (sector.performance <= 0) {
+        // this.value.style.background = 'rgba(129, 71, 0, .822)';
+        this.valueRate.style.color = 'rgb(141, 15, 15)';
+      } else {
+        // this.value.style.background = 'rgba(100, 0, 231, 0.822)';
+        this.valueRate.style.color = 'rgb(12, 107, 12)';
+      }
+    }
+  }
+
+  valueExitRate(e) {
+    e.preventDefault();
+
+    if (this.state.logic === true) {
+      this.valueRate.style.color = 'rgb(0, 68, 78)';
+    }
+  }
 
   render(){
     const { sector } = this.props; 
 
     return(
       <div ref={e => this.value = e} onPointerEnter={(e) => this.valueEnter(e, sector)} onPointerLeave={(e) => this.valueExit(e)} className='sectorsItem'>
-        <div className='sectorsItem_title'>{sector.name}</div>
-        <div className='sectorsItem_rate'>{(sector.performance * 100).toFixed(2) + '%'}</div>
+        <div className='sectorsItem_inner' >
+          <div className='sectorsItem_title'>{sector.name}</div>
+          <div ref={e => this.valueRate = e} onPointerEnter={(e) => this.valueEnterRate(e, sector)} onPointerLeave={(e) => this.valueExitRate(e)} className='sectorsItem_rate'>{(sector.performance * 100).toFixed(2) + '%'}</div>
+        </div>
       </div>
     )
   }
