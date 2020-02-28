687d186741d323dd6416c6da19be1fe2b2c28b21
Dave <dave.gagnat@gmail.com>
2020-02-28 02:40:12 -0600

    Built securities controller with create and destroy actions

---
+++ app/controllers/api/securities_controller.rb
@@ -0,0 +1,32 @@
+class Api::SecuritiesController < ApplicationController
+  def new 
+    @security = Security.new 
+    render :new 
+  end
+
+  def create 
+    @security = Security.new(security_params)
+
+    if @security.save
+      render json: 'Successfully Added To Watchlist!'
+    else 
+      render json: @security.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @security = Security.find(params[:id])
+
+    if @security.destroy 
+      render json: 'Deleted Security From Watchlist'
+    else
+      render json: 'Sorry Security Not Found'
+    end
+  end
+
+
+  private
+  def security_params
+    params.require(:security).permit(:company_name, :ticker)
+  end
+end
