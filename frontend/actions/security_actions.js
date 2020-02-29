afe42ab83bc95f2227adbb4c5275a5c8495a1f08
Dave <dave.gagnat@gmail.com>
2020-02-29 00:03:20 -0600

    Built security actions, imported util function, created regular, thunk and constants

---
+++ frontend/actions/security_actions.js
@@ -0,0 +1,27 @@
+// Import Local Directory Files
+import * as SecurityApiUtil from '../utils/security_api_util';
+
+// Constraints
+export const ADD_TO_SECURITIES = 'ADD_TO_SECURITIES';
+export const SECURITY_ERRORS = 'SECURITY_ERRORS';
+
+
+// Regular Action Creators
+const gotSecurity = (security) => ({
+  type: ADD_TO_SECURITIES,
+  security
+});
+
+const securityErrors = (errors) => ({
+  type: SECURITY_ERRORS,
+  errors
+});
+
+
+// Thunk Action Creator
+export const addedSecurity = (security) => (dispatch) => (
+  SecurityApiUtil.addSecurity(security).then(
+    securityResponse => dispatch(gotSecurity(securityResponse)),
+    securitiesErrors => dispatch(securityErrors(securitiesErrors))
+  )
+);
