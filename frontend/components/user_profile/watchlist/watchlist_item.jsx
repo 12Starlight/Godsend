9448eb6debc886648b7ee4441b2a8d0c3af5172f
Dave <dave.gagnat@gmail.com>
2020-03-02 07:28:53 -0600

    Updated watchlist, watchlist_item, and watchlist_container to properly pass functions and render information in watchlist. Then styled components for smoothness and look. Then added link to stock profile show page

--- frontend/components/user_profile/watchlist/watchlist_item.jsx
+++ frontend/components/user_profile/watchlist/watchlist_item.jsx
@@ -1,5 +1,6 @@
 // Import React Redux
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Import Local Directory Files
 
@@ -18,13 +19,15 @@ class WatchlistSecurityItem extends React.Component {
     const { security } = this.props; 
 
     return(
-      <article className='watchlistItem_container_inner'>
+      <article className=''>
+      <Link className='watchlistItem_container_inner' to={`/stock/${security.symbol}`}>
         <div className='watchlistItem_company' >{security.company}</div>
         <div className='watchlistItem_data'>
           <div>{security.symbol}</div>
           <div>{security.latestPrice}</div>
-          <div className='watchlistItem_priceChange'>{(security.priceChange * 100).toFixed(1) + '%'}</div>              
-        </div>
+          <div className='watchlistItem_priceChange'>{(security.priceChange * 100).toFixed(1) + '%'}</div>
+        </div>      
+      </Link>
       </article>
     )
   }
